f3d3025bb0797062a85864d77a2f208eb9a0bc0e
OscarMG55 <73046213+OscarMG55@users.noreply.github.com>
2022-05-17 07:58:08 -0500

    17-05-2022

---
+++ 03_Interfaz_Funcional/Examen_Practico_Unidad_3/Prueba_Examen.java
@@ -0,0 +1,25 @@
+public class Prueba_examen{
+	
+	public void mensaje_hola(){
+
+	System.out.println("Hola mi nombre es Oscar Matinez Gonzalez");
+
+	}
+
+	public void mensaje hola(String contenido){
+
+	contenido.mensaje_hola();
+
+	System.out.println("La cadena es:"+contenido);
+	}
+
+	public static operacion_prueba(int x,int y){
+
+		suma=(int x, int y);
+		return (suma);
+	}
+
+	public abstract operacion_prueba(int n1, int n2, int n3){
+		
+	}
+}
